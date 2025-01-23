183a3df661c5dfadbcbb210767c1dc81a5c00849
hirbodh <hirbodh221@gmail.com>
2025-01-23 11:37:16 +0330

    feach - add deepgram

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Recognition from './Component/Recognition';
+import Deepgram from './Component/Deepgram';
 
 function App() {
   return (
---
+++ src/Component/Deepgram.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Deepgram() {
+  return (
+    <div>
+        Set Up API Keys: Ensure you have access to your Deepgram API key (and optionally, DeepL API key) and set them as environment variables:
+    </div>
+  )
+}
